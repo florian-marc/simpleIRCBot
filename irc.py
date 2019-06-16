79fcadceea30ef6863745985a791ab171003e51d
florian-marc <florian.marc@devinci.fr>
2019-06-17 00:39:17 +0200

    Added a simple connection method

--- irc.py
+++ irc.py
@@ -45,11 +45,30 @@ class IRC:
         self.irc.send(bytes("USER " + botNick + " " + botNick +
                             " " + botNick + " :" + botRealName + "\n", "UTF-8"))
         time.sleep(5)
-    
-    def joinChannel (self, channel):
+
+    def joinChannel(self, channel):
         """Joins a channel
-        
+
         Args:
+
             channel (string): channel to communicate on
         """
         self.irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
+
+    def simpleConnect(self, server, port, channel, botNick, botRealName=""):
+        """Performs a connection to a server, authentication procedure and channel connection
+
+        Args:
+
+            server (string): server address
+            port (integer): server port
+            channel (string): channel to communicate on
+            botNick (string): nickname of the bot
+            botRealName (string): bot real name. Defaults to botNick.
+        """
+        self.connect(server, port)
+        if(botRealName != ""):
+            self.authentify(botNick, botRealName)
+        else:
+            self.authentify(botNick, botNick)
+        self.joinChannel(channel)
